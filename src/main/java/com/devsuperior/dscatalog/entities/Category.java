3751c757da5ab2010fd056103b88b482df31bfdc
naianebatista <nbatista@ciandt.com>
2022-09-12 16:52:37 -0300

    categoryResource

--- src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -4,22 +4,22 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Category implements Serializable {
-    private String id;
+    private Long id;
     private String name;
 
     public Category() {
     }
 
-    public Category(String id, String name) {
+    public Category(Long id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    public String getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -29,16 +29,18 @@ public class Category implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof Category)) return false;
 
         Category category = (Category) o;
 
         if (!Objects.equals(id, category.id)) return false;
         return Objects.equals(name, category.name);
     }
+
     @Override
     public int hashCode() {
         int result = id != null ? id.hashCode() : 0;
