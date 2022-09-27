f44e181dd071c46f0979a5ae322d0c713ab412bf
naianebatista <nbatista@ciandt.com>
2022-09-27 11:17:17 -0300

    Excessoes  lançadas

--- src/main/java/com/devsuperior/dscatalog/resources/categoryResource.java
+++ src/main/java/com/devsuperior/dscatalog/resources/categoryResource.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dscatalog.resource;
+package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
@@ -6,6 +6,7 @@ import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +24,10 @@ public class categoryResource {
         List<CategoryDTO>list = categoryService.findAll();
         return ResponseEntity.ok().body(list);
     }
+    @GetMapping(value = "/{id}")
+   public ResponseEntity<CategoryDTO>findById(@PathVariable Long id){
+        CategoryDTO dto = categoryService.findById(id);
+        return ResponseEntity.ok().body(dto);
 
+    }
 }
--- src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -3,25 +3,35 @@ package com.devsuperior.dscatalog.services;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
+	
     @Autowired
    private CategoryRepository categoryRepository;
+    
     @Transactional(readOnly = true)
     public List<CategoryDTO> findAll(){
         List<Category>list= categoryRepository.findAll(); //preciso converter a List<Category pra list<CategoryDto> :
 
       return list.stream()
                 .map(x-> new CategoryDTO(x)).collect(Collectors.toList()); //transformando stream em list
-
-
     }
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id){
+    
+        Optional<Category> obj = categoryRepository.findById(id);
+        Category entity =obj.orElseThrow(() -> new EntityNotFoundException("Entity Not Found"));
+        
+        return new CategoryDTO(entity);
+    }
+
 }
