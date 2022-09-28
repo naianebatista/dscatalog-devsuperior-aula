49e444e7d3e7a7c142d93a81399d5343c65749f4
naianebatista <nbatista@ciandt.com>
2022-09-28 09:22:38 -0300

    update category

--- src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -3,11 +3,12 @@ package com.devsuperior.dscatalog.services;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -29,7 +30,7 @@ public class CategoryService {
     public CategoryDTO findById(Long id){
     
         Optional<Category> obj = categoryRepository.findById(id);
-        Category entity =obj.orElseThrow(() -> new EntityNotFoundException("Entity Not Found"));
+        Category entity =obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
         
         return new CategoryDTO(entity);
     }
@@ -39,5 +40,18 @@ public class CategoryService {
         entity.setName(dto.getName());
         entity = categoryRepository.save(entity);
         return new CategoryDTO(entity);      //converto entity novamente p retornar  CategoryDTO
+    }
+    @Transactional
+    public CategoryDTO update(Long id,CategoryDTO dto) {
+        try {
+            Category entity = categoryRepository.getReferenceById(id);
+            entity.setName(dto.getName());
+            entity = categoryRepository.save(entity);
+            return new CategoryDTO(entity);
+        }
+        catch (EntityNotFoundException e){
+                throw new ResourceNotFoundException("Id not found" +id);
+        }
+
     }
 }
